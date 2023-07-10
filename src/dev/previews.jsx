53c01ddcb0a9a458ecdeb77c75a6ce2ffafd5728
Lorenzo Bernini <templarec@gmail.com>
2023-07-10 13:04:22 +0200

    feat: bookmarks

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
-import {ContextProvider} from "../Contexts/index.jsx";
+import {ContextProvider} from "../Contexts/ContextProvider.jsx";
 
 const ComponentPreviews = () => {
     return (
